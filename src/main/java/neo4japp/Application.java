92038d32eaf9d377dde89a2daa6a71078cb24846
David Brown <dbrown@verisk.com>
2019-03-11 18:19:38 -0500

    Initial commit

---
+++ src/main/java/neo4japp/Application.java
@@ -0,0 +1,10 @@
+package neo4japp;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
